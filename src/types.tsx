87931a9027eae01a7a4b0802c82389933a8f2f5b
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-08-01 11:24:48 -0400

    add types

--- src/types.tsx
+++ src/types.tsx
@@ -6,3 +6,20 @@ export type ComponentPropsType = {
   className?: string;
   children: ReactNode;
 };
+
+export type SearchParamsType = {
+  [key: string]: string | string[] | undefined;
+};
+
+export interface Song {
+  id: string;
+  title: string;
+  artist: string;
+  playURL?: string;
+  cover: string | null;
+}
+
+export interface DisplaySong extends Song {
+  displayCover: string | null;
+  story: string;
+}
