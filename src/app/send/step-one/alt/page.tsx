579d7576210ef8853dbb79ecf59d5bdc7026ac97
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-08-01 11:25:01 -0400

    update views

---
+++ src/app/send/step-one/alt/page.tsx
@@ -0,0 +1,14 @@
+/** @format */
+import Card from "@/components/card";
+import Spark from "@/components/spark";
+
+export default function SendViewStepTwo() {
+  return (
+    <>
+      <Card className="flex flex-col gap-4">alt</Card>
+      <div className="flex justify-center -mb-20 mt-20">
+        <Spark href="/receive" />
+      </div>
+    </>
+  );
+}
--- src/app/send/step-one/page.tsx
+++ src/app/send/step-one/page.tsx
@@ -2,15 +2,20 @@
 
 import Card from "@/components/card";
 import { TapeLabel } from "@/components/cassette";
-import { TitleInput } from "@/components/input";
+import { SearchInputDisplay } from "@/components/input";
 import Spark from "@/components/spark";
 
-export default function SendViewStepOne() {
+export default async function SendViewStepOne() {
   return (
     <>
-      <Card className="flex flex-col gap-4">
-        <TapeLabel index={1} text="what is a song that is meaningful to you?" />
-        <TitleInput />
+      <Card className="flex flex-col justify-between">
+        <div className="flex flex-col gap-4 h-full">
+          <TapeLabel
+            index={1}
+            text="what is a song that is meaningful to you?"
+          />
+          <SearchInputDisplay />
+        </div>
       </Card>
       <div className="flex justify-center -mb-20 mt-20">
         <Spark href="/send/step-two" />
