b86bd7836684f17ab3310111933499595a8161ae
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-08-01 11:23:47 -0400

    update components

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -4,8 +4,17 @@
 import useFormStore from "@/store/form";
 import { usePathname, useRouter } from "next/navigation";
 import { startTransition, useState } from "react";
+import { FormEvent } from "react";
+import { getSong } from "@/services/songs";
+import { Song } from "@/types";
+import { InputSong } from "./vinyl";
+import Link from "next/link";
 
-export const TitleInput = () => {
+interface InputProps {
+  name: string;
+}
+
+export const TitleInput = ({ name }: InputProps) => {
   const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>();
   const pathname = usePathname();
   const router = useRouter();
@@ -13,8 +22,8 @@ export const TitleInput = () => {
   return (
     <input
       type="text"
-      name="text"
-      placeholder="type & search"
+      name={name}
+      placeholder="paste track ID here"
       onChange={(e) => {
         clearTimeout(timeoutId);
         let id = setTimeout(() => {
@@ -27,7 +36,7 @@ export const TitleInput = () => {
         }, 500);
         setTimeoutId(id);
       }}
-      className="border rounded-lg py-2 px-4 italic bg-white text-black"
+      className="border rounded-full py-2 px-4 bg-white text-black w-4/5"
     />
   );
 };
@@ -36,12 +45,61 @@ export const TextInput = () => {
   const { inputText, setInputText } = useFormStore();
 
   return (
+    <textarea
+      placeholder={inputText}
+      onChange={(e) => {
+        setInputText(e.target.value);
+      }}
+      className="border h-3/4 overflow-scroll resize-none w-full bg-white text-black py-2 px-4 text-xs rounded-lg"
+    />
+  );
+};
+
+export const SearchInputDisplay = () => {
+  const [song, setSong] = useState<Song | undefined>(undefined);
+  const [error, setError] = useState(false);
+
+  async function searchSong(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const trackId = formData.get("song") as string;
+    const response = await getSong({ trackId, setError });
+    setSong(response);
+    setError(false);
+  }
+
+  return (
     <>
-      <textarea
-        placeholder={inputText}
-        onChange={(e) => setInputText(e.target.value)}
-        className="border h-3/4 overflow-scroll resize-none w-full bg-white text-black py-2 px-4 text-xs rounded-lg"
-      />
+      <form
+        onSubmit={searchSong}
+        name="song"
+        className="flex justify-between gap-2"
+      >
+        <TitleInput name="song" />
+        <button className="w-1/5 border rounded-full hover:bg-black">
+          submit
+        </button>
+      </form>
+      <div className="py-2">
+        {error && (
+          <span className="px-12 opacity-75 text-xl">
+            no song with this track ID is found (⋟﹏⋞) ...
+          </span>
+        )}
+        {song ? (
+          <InputSong song={song} />
+        ) : (
+          <div className="flex justify-end text-xs opacity-50">
+            can't find the song you are looking for?&nbsp;
+            <Link
+              className="rounded-full border hover:bg-black px-2"
+              href="/send/step-one/alt"
+            >
+              add it manually
+            </Link>
+          </div>
+        )}
+      </div>
     </>
   );
 };
--- src/components/vinyl.tsx
+++ src/components/vinyl.tsx
@@ -1,30 +1,26 @@
 /** @format */
 
 import Image from "next/image";
-
-interface Song {
-  title: string;
-  artist: string;
-  story: string;
-  coverURL: string;
-}
+import { DisplaySong, Song } from "@/types";
 
 const SongVinyl = ({
   title,
-  coverURL,
+  displayCover,
 }: {
   title: string;
-  coverURL: string;
+  displayCover: string | null;
 }) => {
   return (
     <div className="flex items-center justify-center border border-gray-200/50 rounded-full w-[250px] h-[250px] bg-black">
       <div className="w-[100px] h-[100px] border border-gray-200/50 drop-shadow-vinyl rounded-full overflow-hidden">
-        <Image
-          src={coverURL}
-          width={100}
-          height={100}
-          alt={`cover art for ${title}`}
-        />
+        {displayCover && (
+          <Image
+            src={displayCover}
+            width={100}
+            height={100}
+            alt={`cover art for ${title}`}
+          />
+        )}
       </div>
     </div>
   );
@@ -37,30 +33,54 @@ const SongDescriptionCard = ({
 }: {
   title: string;
   artist: string;
-  story: string;
+  story?: string;
 }) => {
   return (
     <div className="flex flex-col">
       <div className="flex flex-col font-mono justify-between text-end border-b pb-4">
-        <div className="text-3xl">{title}</div>
+        <div className="text-2xl max-w-100 truncate text-green-50">{title}</div>
         <div className="text-sm opacity-75 after:italic">{artist}</div>
       </div>
-      <div className="border h-full rounded-lg py-2 px-4 text-xs mt-8 overflow-y-scroll bg-white bg-opacity-20 max-w-[300px]">
-        {story}
-      </div>
+      {story && (
+        <div className="border h-full rounded-lg py-2 px-4 text-xs mt-8 overflow-y-scroll bg-white bg-opacity-20 max-w-[300px]">
+          {story}
+        </div>
+      )}
     </div>
   );
 };
 
-export const SongDisplay = ({ song }: { song: Song }) => {
+export const SongDisplay = ({ song }: { song: DisplaySong }) => {
   return (
     <div className="flex flex-row w-full justify-between">
-      <SongVinyl title={song.title} coverURL={song.coverURL} />
+      <SongVinyl title={song.title} displayCover={song.displayCover} />
       <SongDescriptionCard
         title={song.title}
         artist={song.artist}
         story={song.story}
       />
+    </div>
+  );
+};
+
+export const InputSong = ({ song }: { song: Song }) => {
+  return (
+    <div className="flex justify-between">
+      <div className="rounded-full border bg-black bg-opacity-50 flex p-2 mx-2 justify-between w-fit gap-20">
+        {song.cover && (
+          <div className="rounded-full min-w-[80px] border drop-shadow-vinyl overflow-hidden">
+            <Image
+              src={song.cover}
+              width={80}
+              height={80}
+              alt={`cover art for ${song.title}`}
+            />
+          </div>
+        )}
+        <div className="mr-6 max-w-[22vw]">
+          <SongDescriptionCard title={song.title} artist={song.artist} />
+        </div>
+      </div>
     </div>
   );
 };
