55ec2faf2ae1e233921a4d130a031b869edd1ee1
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-08-01 11:24:40 -0400

    create sample view

---
+++ src/lib/examples.tsx
@@ -0,0 +1,18 @@
+/** @format */
+
+export const sampleSong = {
+  id: "3dUipZ8OPRaWetU85XsvDI",
+  title: "xx",
+  artist: "The Millennial Club",
+  cover: "/vinyl.png",
+};
+
+export const sampleDisplaySong = {
+  id: "2IpGJch14myWAYb96GBwP3",
+  title: "vampires",
+  artist: "Tommy Lefroy",
+  story:
+    "if i could send a song back in time as a message to my younger self, i think this might be it. “i just want for you to love your life. i just want for you to not think twice - when somebody asks if you've been doing alright.” back then i worried so endlessly and felt everything so intensely. when i looked into the future sometimes it felt like i was staring into a blackhole. but still, everything eventually turns out alright.",
+  displayCover: "/vampire.jpeg",
+  cover: "/vampire.jpeg",
+};
