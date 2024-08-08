dcedac7a00aa5a1a8861aeafa44bc2bb54b63569
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-08-08 15:43:52 -0400

    remove spark navigation

--- src/app/send/step-two/layout.tsx
+++ src/app/send/step-two/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         </div>
       </Card>
       <div className="flex justify-center -mb-20 mt-20">
-        <Spark href="/receive" />
+        {/* <Spark href="/receive" /> */}
       </div>
     </>
   );
