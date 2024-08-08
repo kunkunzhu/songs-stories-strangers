318cae18d5615f7cdac875f75e41d10d22f664f0
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-08-08 15:42:00 -0400

    block receive view

--- src/app/receive/page.tsx
+++ src/app/receive/page.tsx
@@ -1,9 +1,28 @@
 /** @format */
 
 import Card from "@/components/card";
+import { InputLabel } from "@/components/input";
 import Spark from "@/components/spark";
 import { SongDisplay } from "@/components/vinyl";
 import { sampleDisplaySong } from "@/lib/examples";
+import Link from "next/link";
+
+const ReceiveViewTemp = () => {
+  return (
+    <div className="flex flex-col gap-4 text-xl mx-auto my-auto align-middle text-left">
+      <div>
+        u are here early ... <br />
+        pls go send a song instead to help populate the database ദ്ദി ( ᵔ ᗜ ᵔ )
+      </div>
+      <Link href="/send/step-one">
+        <InputLabel
+          name="send a song"
+          className="uppercase text-xl w-fit px-6 py-2 hover:drop-shadow-spark"
+        />
+      </Link>
+    </div>
+  );
+};
 
 export default function ReceiveView() {
   return (
@@ -11,7 +30,8 @@ export default function ReceiveView() {
       <div>
         <Card>
           <div className="flex h-full w-full">
-            <SongDisplay song={sampleDisplaySong} />
+            {/* <SongDisplay song={sampleDisplaySong} /> */}
+            {ReceiveViewTemp()}
           </div>
         </Card>
         <div className="flex justify-center items-center -mb-20 mt-20">
