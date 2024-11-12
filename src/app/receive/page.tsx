f6468c0a10a8fdfc4c76a6aafcd809cd98201501
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-11-11 22:58:16 -0500

    create receive ui

--- src/app/receive/page.tsx
+++ src/app/receive/page.tsx
@@ -1,11 +1,16 @@
 /** @format */
+"use client";
 
 import Card from "@/components/card";
 import { InputLabel } from "@/components/input";
-import Spark from "@/components/spark";
 import { SongDisplay } from "@/components/vinyl";
 import { sampleDisplaySong } from "@/lib/examples";
+import { receiveSong } from "@/services/songs";
+import { DisplaySong } from "@/types";
 import Link from "next/link";
+import { BarLoader } from "react-spinners";
+import { useEffect, useState } from "react";
+import { TapeLink } from "@/components/cassette";
 
 const ReceiveViewTemp = () => {
   return (
@@ -25,17 +30,45 @@ const ReceiveViewTemp = () => {
 };
 
 export default function ReceiveView() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [song, setSong] = useState<DisplaySong>(sampleDisplaySong);
+
+  useEffect(() => {
+    const receiveSongFromDatabase = async () => {
+      setLoading(true);
+      try {
+        const song: DisplaySong = await receiveSong();
+        setLoading(false);
+        setSong(song);
+      } catch (error) {
+        setError(true);
+        console.error("Error fetching data:", error);
+      } finally {
+        setError(false);
+        setLoading(false);
+      }
+    };
+    receiveSongFromDatabase();
+  }, []);
+
   return (
     <main>
       <div>
         <Card>
           <div className="flex h-full w-full">
-            {/* <SongDisplay song={sampleDisplaySong} /> */}
-            {ReceiveViewTemp()}
+            <div className="my-auto flex mx-auto">
+              <BarLoader width={400} height={2} loading={loading} />
+            </div>
+            {!error && !loading && <SongDisplay song={song} />}
           </div>
         </Card>
         <div className="flex justify-center items-center -mb-20 mt-20">
-          <Spark href="/" />
+          <TapeLink
+            href="/send/step-one"
+            text="send a song"
+            className="rounded-full justify-center font-mono"
+          />
         </div>
       </div>
     </main>
