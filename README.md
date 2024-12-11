73f3c6222697e26a82228448ae9efeeb20e625cf
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-12-10 22:27:53 -0500

    update project description

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 # songs / stories / strangers
 
-I have vivid memories and emotions attached to certain songs. And you might, as well. This is a little web app where you can share a story about a song that is meaningful to you and, in return, read about a song that holds significance for a complete stranger. At the end of the day, we are connected by stories. I cherish the moments when we recognize pieces of ourselves in someone else’s story, even that of a complete stranger, and realize how we are all more connected than we think. 𓈒⟡₊⋆∘
+## about :
+
+What song instantly takes you back to your childhood? What song reminds you of your best friend? What song did you have on-repeat the last time you fell in love?
+
+I have vivid memories and emotions attached to certain songs. And my hunch is that you do, as well. Here, you could send out a song that hold meaning to you and, in return, receive a song that hold meaning for a complete stranger.
+
+The reason that I created this app is because in an age where the music discovery experience is dominated by algorithmic recommendations, it feels serendipitous to discover new songs by learning about the songs that hold significance to someone else. And that, at the end of the day, we are connected by stories. I cherish the moments when we recognize pieces of ourselves in someone else’s story, even that of a complete stranger, and realize how we are all more connected than we think. 𓈒⟡₊⋆∘
+
+## link :
 
 Try the app out [here](https://songs-stories-strangers.vercel.app/)!
 
 ![image](https://github.com/user-attachments/assets/b3821f7e-2e8e-4822-b02c-71992a0ecfbc)
+
+## technologies:
 
 Built with Next.JS, Tailwind CSS, PostgreSQL, Spotify API, Zustand, and the intention to amplify small moments of connection across the vast expanse of the internet 💌
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,25 +7,34 @@ export default function About() {
   return (
     <div>
       <Card>
-        <h1 className="uppercase text-2xl font-mono pb-6">
+        <h1 className="uppercase text-2xl font-mono pb-4">
           🎧 ABOUT THIS PROJECT:
         </h1>
-        <div className="opacity-75 flex flex-col text-sm font-mono gap-4 pb-12">
+        <div className="opacity-75 flex flex-col text-xs font-mono gap-4 pb-6">
           <span>
-            I have vivid memories and emotions attached to certain songs. And
-            you might, as well. This is a little web app where you can share a
-            story about a song that is meaningful to you and, in return, read
-            about a song that holds significance for a complete stranger.{" "}
+            What song instantly takes you back to your childhood? What song
+            reminds you of your best friend? What song did you have on-repeat
+            the last time you fell in love?
           </span>
           <span>
-            At the end of the day, we are connected by stories. I cherish the
-            moments when we recognize pieces of ourselves in someone else’s
+            I have vivid memories and emotions attached to certain songs. And my
+            hunch is that you do, as well. Here, you could send out a song that
+            hold meaning to you and, in return, receive a song that hold meaning
+            for a complete stranger.{" "}
+          </span>
+          <span>
+            The reason that I created this app is because in an age where the
+            music discovery experience is dominated by algorithmic
+            recommendations, it feels serendipitous to discover new songs by
+            learning about the songs that hold significance to someone else. And
+            that, at the end of the day, we are connected by stories. I cherish
+            the moments when we recognize pieces of ourselves in someone else’s
             story, even that of a complete stranger, and realize how we are all
             more connected than we think.
           </span>
         </div>
-        <div className="flex w-full justify-en opacity-50">
-          <span className="text-xs text-end">
+        <div className="flex w-full opacity-25">
+          <span className="text-[0.6rem] text-end">
             © Built by{" "}
             <a
               href="https://kunzhu.vercel.app/"
@@ -34,8 +43,8 @@ export default function About() {
             >
               Kun
             </a>
-            , with the intention to amplify small moments of connection across
-            the vast expanse of the internet 💌
+            , as a way of amplifying the serendipitous moments of connection
+            across this vast expanse of internet 💌
           </span>
         </div>
       </Card>
