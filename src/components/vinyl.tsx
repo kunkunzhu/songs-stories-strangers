53afd06aab05fe80f9f9a58f6f7a05bf10c659db
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-12-10 23:12:18 -0500

    jk i regret prev commit :skull:

--- src/components/vinyl.tsx
+++ src/components/vinyl.tsx
@@ -112,9 +112,7 @@ export const SongDisplay = ({ song }: { song: DisplaySong }) => {
   return (
     <div className="flex flex-col gap-2 md:gap-0 md:flex-row w-full justify-between">
       {song.playURL ? (
-        <a
-          target="_blank"
-          href={playURL}
+        <div
           onMouseEnter={() => playURL && playSong({ playURL })}
           onMouseLeave={() => stopPlayingSong()}
           className="group"
@@ -123,7 +121,7 @@ export const SongDisplay = ({ song }: { song: DisplaySong }) => {
           <div className="hidden group-hover:flex mt-2 w-fit mx-auto text-end transition-all bg-black opacity-20 rounded-full text-xs px-2 py-1">
             {title}
           </div>
-        </a>
+        </div>
       ) : (
         <div>
           <SongVinyl title={title} />
